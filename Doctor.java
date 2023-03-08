7ff8858e79350938fde9d49923e7b3bb2258892a
Andrew Williams <alexissanchezd97@gmail.com>
2023-03-07 20:50:58 -0500

    Vimos un poco mas de como funciona el metodo constructor y experimentamos tantito con el this.atributo

--- Doctor.java
+++ Doctor.java
@@ -4,6 +4,14 @@ public class Doctor {
     String name;
     String speciality;
     //5.3 Agregamos los comportamientos
+
+    //6.0 Recreamos el metodo constructor
+    Doctor(String name){
+        /*6.1.1 Aqui podemos ver como debemos declarar que la variable para que
+        coloque el nuevo atributo de la manera adecuada*/
+        this.name = name;
+        System.out.println("El nombre del doctor es: " + this.name);
+    }
     public void showName(){
         System.out.println(name);
     }
--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     //5.0 Como en todo proyecto de java necesitamos un metodo name para que el proyecto pueda vivir
     public static void main(String[] args){
         //5.4 Aqui vemos un ejemplo sencillo de como se usan los atributos de una clase modularizada
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Alejandro Sanchez";
+        /*6.1 Aquí agregamos la funcion de que nosotros le pasamos el parametro
+        y el metodo constructor lo añade como atributo a la nueva instancia utilizanod el atributo
+        this.name = name que usamos en el metodo contructos*/
+        Doctor myDoctor = new Doctor("Alejandro Sanchez");
         myDoctor.showName();
     }
 }
