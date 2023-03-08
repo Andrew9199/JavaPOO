10c18f1a890ed6dfb0d6bc2fba6f0018493c01bf
Alexis Sanchez <alexissanchezd97@gmail.com>
2023-03-08 12:49:03 -0500

    11 Reto creación de la clase patient con su metodo constructor

---
+++ Patient.java
@@ -0,0 +1,15 @@
+//Reto 11
+public class Patient{
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    double weight;
+    double height;
+    String blood;
+
+    public static void Patient(String name,String email){
+
+    }
+}
