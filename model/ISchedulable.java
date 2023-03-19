24e1e43f8b83bd497be50640c5d818b95178762d
Andrew Williams <alexissanchezd97@gmail.com>
2023-03-18 20:41:51 -0500

    Cr24 eacion de l la clase nurse junto con los metodos especiales para nurse y doctor junto con la interfaz de ISchedulable 2

--- model/ISchedulable.java
+++ model/ISchedulable.java
@@ -1,2 +1,9 @@
-package model;public interface ISchedulable {
+package model;
+
+import java.util.Date;
+
+public interface ISchedulable {
+//20.1 Aqui agregamos el identificador de nuestra interfaz para poderla implementar en las clases
+    void schedule(Date date, String time);
+
 }
